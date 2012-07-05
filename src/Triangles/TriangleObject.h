4e412819f96498306c8d1c3eca261c1c58f70422
piligrimo <piligrimo@googlemail.com>
2012-07-05 12:43:15 +0000

    classes for handling triangles

---
+++ src/Triangles/TriangleObject.h
@@ -0,0 +1,50 @@
+/*
+ *  TriangleObject.h
+ *  TuioPad
+ *
+ *  Created by Oleg Langer on 01.06.12
+ *  Copyright 2011 Oleg Langer. All rights reserved.
+ *
+ */
+#ifndef TuioPad_TriangleObject_h
+#define TuioPad_TriangleObject_h
+#include "SimpleTriangle.h"
+
+
+class TriangleObject
+{
+private:
+    float x, y;
+    float angle;
+    
+public:
+	TriangleObject() {
+		isAlive = false;
+		wasAlive = false;
+		moved = false;
+	};
+    TriangleObject(SimpleTriangle* tr);
+	~TriangleObject(){};
+	
+    
+    SimpleTriangle *triangle;
+    
+	
+	bool isAlive;		// is it alive this frame
+	bool wasAlive;		// was it alive this frame
+	bool moved;			// did it move this frame
+
+	
+	void update();
+    float getX();
+    float getY();
+    float getAngle();
+    void setTriangle(SimpleTriangle *);
+    int getSymbolID();
+    
+    string testOutput();
+};
+
+
+
+#endif // TuioPad_TriangleObject_h
